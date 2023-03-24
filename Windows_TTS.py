49326f035bb0dac403824014d24e306576351b3a
A3Art <122574717+Artem-Mironov-a3art@users.noreply.github.com>
2023-03-24 22:17:29 +0300

    Версия 0.0.0 Бета

--- Windows_TTS.py
+++ Windows_TTS.py
@@ -1,10 +1,55 @@
 import tkinter as tk
+from tkinter import ttk
+import pyttsx3
+
+tts = pyttsx3.init()
+voice = tts.getProperty("voices")
+list_voice =[]
+for i in voice:
+    list_voice.append(i.name)
+
+def set_voice():
+    name_v = voice_menu.get()
+    for vc in voice:
+        if vc.name == name_v:
+            tts.setProperty("voice", vc.id)
+
+def save_mp3():
+    file_name = Ent.get()
+    file_save = EntF.get()
+    tts.save_to_file(file_name, file_save)
+    tts.runAndWait()
 
 win = tk.Tk()
-win.title("TTS_Windows")
+win.title("Windows_TTS")
 win.geometry("640x480")
 
 label_Name = tk.Label(win, text="The program that translates txt files into mp3")
 label_Name.pack()
+
+label_rfile = tk.Label(win, text="Specify the path to the file")
+label_rfile.pack()
+
+Ent = tk.Entry(win, width=45)
+Ent.pack()
+
+label_menu = tk.Label(win, text="Select a voice for text-to-speech")
+label_menu.pack()
+
+voice_menu = ttk.Combobox(win, values=list_voice, width=45)
+voice_menu.current(0)
+voice_menu.pack()
+
+btn_set_voice = tk.Button(text="Set voice", command=set_voice)
+btn_set_voice.pack()
+
+save_label = tk.Label(text="Enter the address and name to save the file")
+save_label.pack()
+
+EntF = tk.Entry(win, width=45)
+EntF.pack()
+
+btn_save = tk.Button(text="Save mp3", command=save_mp3)
+btn_save.pack()
 
 win.mainloop()
